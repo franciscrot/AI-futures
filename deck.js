33aa6a16fbd2f830c00a241ee3c41861a1d0de8f
Jo Lindsay Walton <franciscrot@gmail.com>
2026-07-31 13:52:46 +0100

    Revise Dennis outcome card

--- deck.js
+++ deck.js
@@ -808,7 +808,7 @@ window.deck = [
     subplotPath: "path-1-2",
     subplotStage: 3,
     subplotPosition: 63,
-    description: "The organisation fills with model employees. Performance scores soar. Working styles and work personae steadily converge.",
+    description: "• You have been doing your best to combat AI-powered performancemaxxing by protecting less measurable ways of working.\n• You notice that Dennis has now pushed through a series of “protecting the unquantifiable” KPIs and metrics, which everyone is busy pursuing.",
   },
   {
     id: 113,
